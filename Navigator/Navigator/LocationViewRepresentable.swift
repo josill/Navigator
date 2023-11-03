1a5883161ff0c8da7c8a165a659aefb8120bf53f
josill <josill@ttu.ee>
2023-11-03 12:27:05 +0200

    polyline using swiftui

---
+++ Navigator/Navigator/LocationViewRepresentable.swift
@@ -0,0 +1,13 @@
+//
+//  LocationViewRepresentable.swift
+//  Navigator
+//
+//  Created by Jonathan Sillak on 03.11.2023.
+//
+
+import SwiftUI
+import MapKit
+
+//struct LocationViewRepresentable: UIViewRepresentable {
+//    typealias UIViewType = <#type#>
+//}
---
+++ Navigator/Navigator/NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigatorApp.swift
+//  Navigator
+//
+//  Created by Jonathan Sillak on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
